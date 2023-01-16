5408096d5ee0b6b9a8cb3dbdf45d47b58668178b
vivek kumar <vivekumar190@gmail.com>
2023-01-16 17:39:43 +0530

    mobile responsive

--- src/components/form.js
+++ src/components/form.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import "../App.css";
 import Select from "react-select";
-
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const options = [
   { value: "Information Technology", label: "Information Technology" },
   { value: "Oil and Natural Gas", label: "Oil and Natural Gas" },
@@ -14,12 +17,50 @@ const sizeoptions = [
   { value: "201-500", label: "201-500" },
   { value: "500+", label: "500+" },
 ];
+const formSchema = Yup.object({
+  companyname: Yup.string().required("companyname is required"),
+  industry: Yup.string().required("industry is required"),
+});
 const Form = () => {
   const [selectedOption, setSelectedOption] = useState(null);
   const [selectedSizeOption, setSelectedSizeOption] = useState("1 - 20");
+
+  // formik
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+      toast(`🦄 Wow so easy! ${values}`, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    },
+    validationSchema: formSchema,
+  });
+  const notify = () =>
+    toast(`🦄 Wow so easy!`, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
   return (
-    <div className="form">
+    <form className="form" onSubmit={formik.handleSubmit}>
       <div className="header">
+        <ToastContainer />
         <img src="/images/Solid_lines.svg"></img>
         <div className="title">It’s a delight to have you onboard</div>
         <div className="subtitle">
@@ -28,7 +69,16 @@ const Form = () => {
         </div>
         <div className="inputdiv">
           Company name
-          <input className="input" placeholder=" e.g. Example Inc"></input>
+          <input
+            className="input"
+            placeholder=" e.g. Example Inc"
+            value={formik.values.companyname}
+            onChange={formik.handleChange("companyname")}
+            onBlur={formik.handleBlur("companyname")}
+          ></input>
+          <div className="errortext">
+            {formik.touched.companyname && formik.errors.companyname}
+          </div>
         </div>
         <div className="inputdiv">
           Industry
@@ -37,7 +87,13 @@ const Form = () => {
             defaultValue={selectedOption}
             onChange={setSelectedOption}
             options={options}
+            value={formik.values.industry}
+            // onChange={formik.handleChange("companyname")}
+            // onBlur={formik.handleBlur("industry")}
           />
+          <div className="errortext">
+            {formik.touched.industry && formik.errors.industry}
+          </div>
         </div>
         <div className="inputdiv">
           Company size
@@ -56,9 +112,11 @@ const Form = () => {
             ))}
           </div>
         </div>
-        <button className="startedbutton">Get Started</button>
+        <button className="startedbutton" type="submit" onClick={notify}>
+          Get Started
+        </button>
       </div>
-    </div>
+    </form>
   );
 };
 
